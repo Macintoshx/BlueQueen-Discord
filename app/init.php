e41b8392d46640f0ca85e16ba4dc6ad0d8378063
Greg Lieske <grzesiu.lieske@gmail.com>
2016-04-29 23:28:08 +0200

    !pogoda : try-catch

--- app/init.php
+++ app/init.php
@@ -48,20 +48,27 @@ $ws->on(
 
 				if ($message->content == '!pogoda')
 				{
-					$ch = curl_init();
-					// set url
-					curl_setopt($ch, CURLOPT_URL, "https://api.bluequeen.tk/v1/weather?start=-1");
-					//return the transfer as a string
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-					curl_setopt($ch, CURLOPT_USERAGENT,
-						'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-					// $output contains the output string
-					$response = curl_exec($ch);
-					// close curl resource to free up system resources
-					curl_close($ch);
+					try
+					{
+						$ch = curl_init();
+						// set url
+						curl_setopt($ch, CURLOPT_URL, "https://api.bluequeen.tk/v1/weather?start=-1");
+						//return the transfer as a string
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+						curl_setopt($ch, CURLOPT_USERAGENT,
+							'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+						// $output contains the output string
+						$response = curl_exec($ch);
+						// close curl resource to free up system resources
+						curl_close($ch);
 
-					$response = json_decode($response);
-					$resp = "\n[" . $response[0]->Date . "] Wejherowo : " . $response[0]->Value . "°C";
+						$response = json_decode($response);
+						$resp = "\n[" . $response[0]->Date . "] Wejherowo : " . $response[0]->Value . "°C";
+					}
+					catch(Exception $e)
+					{
+						$resp = "\nUsługa chwilowo niedostepna";
+					}
 					$message->reply($resp);
 				}
 
