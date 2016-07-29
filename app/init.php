26ffa63a99dd786a072e6776fcb5bc5ff71060f0
Greg Lieske <grzesiu.lieske@gmail.com>
2016-07-30 00:14:34 +0200

    new version

--- app/init.php
+++ app/init.php
@@ -17,87 +17,59 @@ $bqbot = [
 	'botToken'
 ]; // nazwy zmiennych z hasłami itd.
 
-// Init the Discord instance.
-$discord = new Discord($botToken);
-// Init the WebSocket instance.
-$ws = new WebSocket($discord);
+$discord = new Discord([
+	'token' => 'bot-token',
+]);
 
-$ws->on(
-	'ready',
-	function ($discord) use ($ws)
+$discord->on('ready', function ($discord) {
+	echo "Bot is ready!", PHP_EOL;
+
+	// Listen for messages.
+	$discord->on('message', function ($message, $discord)
 	{
-		// In here we can access any of the WebSocket events.
-		//
-		// There is a list of event constants that you can
-		// find here: https://teamreflex.github.io/DiscordPHP/classes/Discord.WebSockets.Event.html
-		//
-		// We will echo to the console that the WebSocket is ready.
-		echo 'Discord WebSocket is ready!' . PHP_EOL;
-		$discord->getClient()->updatePresence($ws, "with Sad Panda", false);
-		echo 'Game set' . PHP_EOL;
-		// Here we will just log all messages.
-		$ws->on(
-			Event::MESSAGE_CREATE,
-			function ($message, $discord, $newdiscord)
+		echo "{$message->author->username}: {$message->content}",PHP_EOL;
+		if ($message->content == 'ping')
+		{
+			$message->reply('pong!');
+		}
+
+		if ($message->content == '!pogoda')
+		{
+			try
 			{
-				// We are just checking if the message equils to ping and replying to the user with a pong!
-				if ($message->content == 'ping')
-				{
-					$message->reply('pong!');
-				}
+				$ch = curl_init();
+				// set url
+				curl_setopt($ch, CURLOPT_URL, "https://api.bluequeen.tk/v1/weather?start=-1");
+				//return the transfer as a string
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_USERAGENT,
+					'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+				// $output contains the output string
+				$response = curl_exec($ch);
+				// close curl resource to free up system resources
+				curl_close($ch);
 
-				if ($message->content == '!pogoda')
-				{
-					try
-					{
-						$ch = curl_init();
-						// set url
-						curl_setopt($ch, CURLOPT_URL, "https://api.bluequeen.tk/v1/weather?start=-1");
-						//return the transfer as a string
-						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-						curl_setopt($ch, CURLOPT_USERAGENT,
-							'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-						// $output contains the output string
-						$response = curl_exec($ch);
-						// close curl resource to free up system resources
-						curl_close($ch);
-
-						$response = json_decode($response);
-						$resp = "\n[" . $response[0]->Date . "] Wejherowo : " . $response[0]->Value . "°C";
-					}
-					catch(Exception $e)
-					{
-						$resp = "\nUsługa chwilowo niedostepna";
-					}
-					$message->reply($resp);
-				}
-
-				if ($message->content == '!bot')
-				{
-					$message->reply("\nBlueQueen Discord Bot v.0.1\n\nDostepne komendy: \n\nping, !pogoda, !bot");
-				}
-
-				$reply = $message->timestamp->format('d/m/y H:i:s') . ' - '; // Format the message timestamp.
-				$reply .= $message->full_channel->guild->name . ' - ';
-				$reply .= $message->author->username . ' - '; // Add the message author's username onto the string.
-				$reply .= $message->content; // Add the message content.
-				echo $reply . PHP_EOL; // Finally, echo the message with a PHP end of line.
-
+				$response = json_decode($response);
+				$resp = "\n[" . $response[0]->Date . "] Wejherowo : " . $response[0]->Value . "°C";
 			}
-		);
-	}
-);
+			catch(Exception $e)
+			{
+				$resp = "\nUsługa chwilowo niedostepna";
+			}
+			$message->reply($resp);
+		}
 
-$ws->on(
-	'error',
-	function ($error, $ws)
-	{
-		dump($error);
-		exit(1);
-	}
-);
+		if ($message->content == '!bot')
+		{
+			$message->reply("\nBlueQueen Discord Bot v.0.1\n\nDostepne komendy: \n\nping, !pogoda, !bot");
+		}
 
-// Now we will run the ReactPHP Event Loop!
+		$reply = $message->timestamp->format('d/m/y H:i:s') . ' - '; // Format the message timestamp.
+		$reply .= $message->full_channel->guild->name . ' - ';
+		$reply .= $message->author->username . ' - '; // Add the message author's username onto the string.
+		$reply .= $message->content; // Add the message content.
+		echo $reply . PHP_EOL; // Finally, echo the message with a PHP end of line.
+	});
+});
 
-$ws->run();
-
+$discord->run();
